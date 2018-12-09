e6bc60d1eaac4c802ba63db0ae7999ee2f4bb9bd
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2018-12-09 23:21:19 +0900

    スタンダードアセット追加

---
+++ Assets/Scripts/System/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// プレイヤを自動追尾するメインカメラ（正面視点）
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform target;
+	public float height;
+	private Vector3 offset;
+
+	void Start ()
+	{
+		// Playerタグの付いたオブジェクトの位置をtargetとして取得
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+		// カメラとターゲット（プレイヤー)の距離を設定
+		offset = transform.position - target.position;
+	}
+		
+	void LateUpdate ()
+	{
+		// カメラがターゲット（プレイヤー）を見つけてから追いかける（少し遅れて追いかける）
+		transform.position = target.position + offset;
+	}
+}
+
